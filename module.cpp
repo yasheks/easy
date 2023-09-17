d7b783ece2877c1b1446db4e51bac2af8b4d27c8
yasheks <143996832+yasheks@users.noreply.github.com>
2023-09-17 13:14:36 +0300

    Update module.cpp

--- module.cpp
+++ module.cpp
@@ -34,39 +34,7 @@ int itc_revnbr(int num){
     return new_num;
 
 }
-bool itc_iseven(int num) {
-    if(num % 2 == 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-int itc_max(int num, int num2){
-    if (num > num2)
-        return num;
-    else
-        return num2;
-}
-int itc_min(int num, int num2){
-    if (num < num2)
-        return num;
-    else
-        return num2;
-}
-double itc_fmax(double num, double num2) {
-    if (num > num2) {
-        return num;
-    } else {
-        return num2;
-    }
-}
-double itc_fmin(double num, double num2) {
-    if (num < num2) {
-        return num;
-    } else {
-        return num2;
-    }
-}
+
 int itc_sqrt(int num) {
     if (num < 0) {
         return -1;
