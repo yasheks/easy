b1c1c9324162d104d5c02f1f34c9f953f67080b1
yasheks <143996832+yasheks@users.noreply.github.com>
2023-09-17 13:15:32 +0300

    Update module_3.cpp

--- module_3.cpp
+++ module_3.cpp
@@ -1,33 +1,57 @@
-bool itc_iseven(int num) {
-    if(num % 2 == 0) {
-        return true;
-    } else {
-        return false;
+int itc_sqrt(int num) {
+    if (num < 0) {
+        return -1;
     }
+
+    int left = 0;
+    int right = num;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (mid * mid == num) {
+            return mid;
+        }
+
+        if (mid * mid < num) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
 }
-int itc_max(int num, int num2){
-    if (num > num2)
-        return num;
+int itc_skv(int num){
+    int Skv;
+    Skv = num * num;
+    if (num<= 0)
+        return -1;
     else
-        return num2;
+        return Skv;
 }
-int itc_min(int num, int num2){
-    if (num < num2)
-        return num;
+int itc_spr(int a, int b){
+    int Skv;
+    Skv = a * b;
+    if (a<= 0 or b <= 0)
+        return -1;
     else
-        return num2;
+        return Skv;
 }
-double itc_fmax(double num, double num2) {
-    if (num > num2) {
-        return num;
-    } else {
-        return num2;
-    }
+int itc_str(int a, int b, int c) {
+  if (a + b > c && a + c > b && b + c > a) {
+    int p = (a + b + c) / 2;
+    int s = sqrt(p * (p - a) * (p - b) * (p - c));
+    return s;
+  } else {
+    return -1;
+  }
 }
-double itc_fmin(double num, double num2) {
-    if (num < num2) {
-        return num;
-    } else {
-        return num2;
-    }
+double itc_scir(int radius){
+    float pi = 3.14;
+    double Skv = (radius*radius)*pi;
+    if (radius<=0)
+        return-1;
+    else
+        return Skv;
 }
