28064a10d6346fa73437e96471de49ada79a33be
yasheks <143996832+yasheks@users.noreply.github.com>
2023-09-17 13:11:18 +0300

    Update module_2.cpp

--- module_2.cpp
+++ module_2.cpp
@@ -1,1 +1,33 @@
-o# easy
+bool itc_iseven(int num) {
+    if(num % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+int itc_max(int num, int num2){
+    if (num > num2)
+        return num;
+    else
+        return num2;
+}
+int itc_min(int num, int num2){
+    if (num < num2)
+        return num;
+    else
+        return num2;
+}
+double itc_fmax(double num, double num2) {
+    if (num > num2) {
+        return num;
+    } else {
+        return num2;
+    }
+}
+double itc_fmin(double num, double num2) {
+    if (num < num2) {
+        return num;
+    } else {
+        return num2;
+    }
+}
