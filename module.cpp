026f01be4f2995970135040fb5babb15a25bbc63
yasheks <143996832+yasheks@users.noreply.github.com>
2023-09-17 13:16:06 +0300

    Update module.cpp

--- module.cpp
+++ module.cpp
@@ -36,24 +36,4 @@ int itc_revnbr(int num){
 }
 
 
-double itc_pow(int num, int step){
-    int num2 = num;
-    for (int i = 1; i<step; i++){
-        num = num * num2;
-    }
-    return num;
-}
-bool itc_ispositive(int num) {
-    if (num >= 0)
-        return true;
-    else
-        return false;
 
-}
-bool itc_ispositive_d(double num) {
-    if (num >= 0)
-        return true;
-    else
-        return false;
-
-}
